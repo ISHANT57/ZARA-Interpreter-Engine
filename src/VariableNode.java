af61d0c0122d17e5a63acb557b6e0720c0ba0814
Sandhya Kaushal <arshi.kaushal222@gmail.com>
2026-04-12 18:48:11 +0530

    add cmt for users

--- src/VariableNode.java
+++ src/VariableNode.java
@@ -1,10 +1,17 @@
-public class VariableNode implements Expression{
+// VariableNode represents a variable in an expression.
+// It follows the Expression interface so it can be used like other expressions.
+public class VariableNode implements Expression {
+    
+    // This stores the name of the variable (like "x" or "y").
     private final String name;
     
+    // Constructor: when you create a VariableNode, you give it a name.
     public VariableNode(String name) { 
         this.name = name; 
     }
 
+    // The evaluate method looks up the variable's value in the Environment.
+    // It asks the Environment for the value that matches the variable's name.
     @Override
     public Object evaluate(Environment env) { 
         return env.get(name); 
